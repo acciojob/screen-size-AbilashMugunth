0182e6474b509e13b542e318cbd2f79e2e626518
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-09 21:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let width = document.querySelector("#width");
+let height = document.querySelector("#height");
+
+width.innerHTML = this.innerWidth;
+height.innerHTML = this.innerHeight;
+
+this.addEventListener("resize", (event) => {
+  console.log(event.target.innerHeight, event.target.innerWidth);
+
+  width.innerHTML = event.target.innerWidth;
+  height.innerHTML = event.target.innerHeight;
+});
